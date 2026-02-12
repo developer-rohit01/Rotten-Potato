32fc221b01ff301164bcb01b5dbd9617b4459adf
Rohit <rr5450534@gmail.com>
2026-02-12 23:24:43 +0530

    Add the code upto 2322 hr

--- First.java
+++ First.java
@@ -4,5 +4,14 @@ public class First {
    public static void main(String[] args) {
     
   System.out.println("First Line\nSecond Line");
+
+  boolean student =false;
+
+  if(student){
+    System.out.println("You are a Student");
+  }
+  else{
+    System.out.println("You are NOT a Student");
+  }
 }
 }
