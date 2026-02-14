527743529f17a707b5e881e84f644338c32c067a
Rohit <rr5450534@gmail.com>
2026-02-14 21:39:18 +0530

    Add the padding code using printf)

---
+++ Padding.java
@@ -0,0 +1,16 @@
+public class Padding {
+  public static void main(String[] args) {
+    int number1 = 42;
+    int number2 = 721;
+    int number3 = 745230;
+    int number4 = 75234;
+    int number5 = 7300;
+
+    System.out.printf("%06d\n", number1);
+    System.out.printf("%06d\n", number2);
+    System.out.printf("%06d\n", number3);
+    System.out.printf("%06d\n", number4);
+    System.out.printf("%06d\n", number5);
+
+  }
+}
