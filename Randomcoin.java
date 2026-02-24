7be4142a71b5fecd11658fbc2e7741f4b4d67e35
Rohit <rr5450534@gmail.com>
2026-02-24 19:17:52 +0530

    Update the Randomcoin

--- Randomcoin.java
+++ Randomcoin.java
@@ -4,7 +4,6 @@ public class Randomcoin {
   public static void main(String[] args) {
     Random random = new Random();
     int number;
-    double Num;
     boolean coin;
     coin=random.nextBoolean();
 //    Num=random.nextDouble(1.0, 5.0);
